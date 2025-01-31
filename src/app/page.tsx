5260517b3c311f37423b71a82505826caf201009
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-01-31 10:25:06 +0100

    create routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+import Profile from "./components/profile";
+import About from "./components/About";
 
 const page = () => {
-  return <div>page</div>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignOut />} />
+      <Route path="/profle" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>;
 };
 
 export default page;
