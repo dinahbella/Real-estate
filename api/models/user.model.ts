4579a9dfac4f25769d1c25e5c00e8d47851c68a9
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-01-31 11:42:35 +0100

    user model

--- api/models/user.model.ts
+++ api/models/user.model.ts
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
