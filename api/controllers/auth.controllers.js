8cf6e622abaff0447730899756eb537f3c642ddd
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-02-01 15:26:14 +0100

    feet

---
+++ api/controllers/auth.controllers.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
+
+export const signup = async (req, res) => {
+  const {name, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword
+  });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+
+};
+
+export const signin = (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,3 +17,10 @@ mongoose
 app.listen(4000, () => {
   console.log("server is running on port 4000");
 });
+
+app.get("/test", (req, res) => {
+  res.send("Hello Dinah");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
